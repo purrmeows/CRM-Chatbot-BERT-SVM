db276f7efddd0b30d13bd518e0a68a00ccbd990e
purrmeows <mewspurr.lmine07@gmail.com>
2025-09-28 21:12:55 +0700

    Adjsut Port and Version

--- app.py
+++ app.py
@@ -79,5 +79,5 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
